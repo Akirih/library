4916cbeaf95de869b6b5a12f1142d8322832fa80
Akirih <mauropinto@live.com.pt>
2023-06-13 00:10:42 +0100

    add book popup finished (html broken temporarily)

---
+++ script.js
@@ -0,0 +1,22 @@
+
+document.querySelector('.book--add').addEventListener('click', () => console.log(event) )
+
+let myLibrary = [];
+
+/*  */
+
+function book(){
+
+}
+
+function addBook(){
+
+}
+
+function removeBook () {
+
+}
+
+function displayBook(){
+
+}
