1be20697e840df9d28dd3f49fce457da412192d4
Akirih <mauropinto@live.com.pt>
2023-07-03 13:39:14 +0100

    finished

--- script.js
+++ script.js
@@ -19,13 +19,13 @@ let popup = `<div class="add--book--popup--fade">
             <div class="form--progress--container">
                 <label class="progress--label">Progress</label>
                 <div class="progress--bar">
-                    <div class="progress--reading" onclick="document.getElementById('form--radio--not-started').checked = true; this.style.backgroundColor = 'red';"">
+                    <div class="progress--reading" onclick="progressUpdate.call(this, 1);getElementById('form--radio--not-started').checked = true">
                         <input type="radio" id="form--radio--not-started" name="form--radio--status" value="Not Started">
                     </div>
-                    <div class="progress--on-hold" onclick="document.getElementById('form--radio--started').checked = true; this.style.backgroundColor = 'yellow';">
+                    <div class="progress--on-hold" onclick="progressUpdate.call(this, 2);document.getElementById('form--radio--started').checked = true;">
                         <input type="radio" id="form--radio--started" name="form--radio--status" value="Reading">
                     </div>
-                    <div class="progress--finished" onclick="document.getElementById('form--radio--finished').checked = true;this.style.backgroundColor = 'green';">
+                    <div class="progress--finished" onclick="progressUpdate.call(this, 3);document.getElementById('form--radio--finished').checked = true">
                         <input type="radio" id="form--radio--finished" name="form--radio--status" value ="Finished">
                        
                     </div>                            
@@ -57,8 +57,7 @@ function addBook(){
 }
 
 function removeBook () {
-    console.log(this.dataset.index);
-    myLibrary.splice(this.index, 1);
+    myLibrary.splice(this.dataset.index,1);
     this.parentNode.parentNode.parentNode.remove();
     displayBook();
 }
@@ -69,6 +68,15 @@ function displayBook(){
     while (addButton.previousSibling) addButton.previousSibling.remove();
 
     for (; i< myLibrary.length; i++){
+
+        let aColor;
+        let bColor;
+        let cColor;
+
+        if (myLibrary[i].progress == 'Not Started') {aColor = 'red';}
+        else if (myLibrary[i].progress == 'Reading') {bColor = 'yellow';}
+        else if (myLibrary[i].progress == 'Finished') {cColor = bColor = aColor = 'green';}
+
         document.getElementById('book--list--container').insertAdjacentHTML("afterbegin", `
         <div class="book--card">
             <div class="book-details">
@@ -80,11 +88,11 @@ function displayBook(){
             </div>
             <div class="book--progress">
                 <div class="progress--bar">
-                    <div class="progress--reading"></div>
-                    <div class="progress--on-hold"></div>
-                    <div class="progress--finished"></div>
+                    <div class="progress--reading" style="background-color:${aColor};" onclick="progressUpdateRealTime.call(this, 1);" data-index="${i}"></div>
+                    <div class="progress--on-hold" style="background-color:${bColor};" " onclick="progressUpdateRealTime.call(this, 2);" data-index="${i}"></div>
+                    <div class="progress--finished" style="background-color:${cColor};" " onclick="progressUpdateRealTime.call(this, 3);" data-index="${i}"></div>
                 </div>
-                <p class="progress--text">Not Started</p>
+                <p class="progress--text">${myLibrary[i].progress}</p>
             </div>
             </div>
         `);
@@ -92,31 +100,32 @@ function displayBook(){
     addDelete();
 }
 
-function displayBookTWO(){    
-    let i = 0;
-    myLibrary.map(x => {
-        document.getElementById('book--list--container').insertAdjacentHTML("afterbegin", `
-        <div class="book--card">
-            <div class="book-details">
-                <h2 class="book--author">${x.name}<img src="img/x-circle.svg" class="remove--button" data-index="${i}"></h2>
-                <div class="author--details">
-                    <p>${x.author}</p>
-                    <p>${x.year}</p>
-                </div>
-            </div>
-            <div class="book--progress">
-                <div class="progress--bar">
-                    <div class="progress--reading"></div>
-                    <div class="progress--on-hold"></div>
-                    <div class="progress--finished"></div>
-                </div>
-                <p class="progress--text">Not Started</p>
-            </div>
-            </div>
-        `);
-        i++;
-    });
-    addDelete();
+
+function progressUpdate (progress){
+    parent = this.parentNode;
+    Array.from(parent.children).forEach( children => children.style.backgroundColor = '')
+    if (progress == 1){ 
+        this.style.backgroundColor = 'red';
+    } else if (progress == 2){
+        this.style.backgroundColor = 'yellow';
+    } else {
+        Array.from(parent.children).forEach( children => children.style.backgroundColor = 'green')
+    }
+}
+
+function progressUpdateRealTime (progress){
+    parent = this.parentNode;
+    Array.from(parent.children).forEach( children => children.style.backgroundColor = '')
+    if (progress == 1){ 
+        this.style.backgroundColor = 'red';
+        myLibrary[this.dataset.index].progress = 'Not Started';
+    } else if (progress == 2){
+        this.style.backgroundColor = 'yellow';
+        myLibrary[this.dataset.index].progress = 'Reading';
+    } else {
+        Array.from(parent.children).forEach( children => children.style.backgroundColor = 'green')
+        myLibrary[this.dataset.index].progress = 'Finished';
+    }    
 }
 
 document.querySelector('.book--add').addEventListener('click', openPopup);
